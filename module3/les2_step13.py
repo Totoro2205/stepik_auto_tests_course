7edc3c927c097db8485865b28284d4a1dc7da107
Totoro2205 <alexander.keepsolid@gmail.com>
2022-02-18 23:41:40 +0200

    lesson 2 step 13 some improvements

--- module3/les2_step13.py
+++ module3/les2_step13.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 import unittest
 
@@ -7,16 +8,16 @@ class TestAbs(unittest.TestCase):
     def test_uniq_selector1(self, url="http://suninjuly.github.io/registration1.html"):
         browser = webdriver.Chrome()
         browser.get(url)
-        input1 = browser.find_element_by_css_selector("div.first_block > div.form-group.first_class > input")
+        input1 = browser.find_element(By.CSS_SELECTOR, ".first_block .first")
         input1.send_keys("Ivan")
-        input2 = browser.find_element_by_css_selector("div.first_block > div.form-group.second_class > input")
+        input2 = browser.find_element(By.CSS_SELECTOR, ".first_block .second")
         input2.send_keys("Petrov")
-        input3 = browser.find_element_by_css_selector("div.form-group.third_class > input")
+        input3 = browser.find_element(By.CSS_SELECTOR, ".first_block .third")
         input3.send_keys("usr@i.ua")
-        button = browser.find_element_by_css_selector("button.btn")
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
         button.click()
         time.sleep(1)
-        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
         welcome_text = welcome_text_elt.text
         self.assertEqual("Congratulations! You have successfully registered!", welcome_text,
                          msg='Registration error!!!')
@@ -24,20 +25,19 @@ class TestAbs(unittest.TestCase):
     def test_uniq_selector2(self, url="http://suninjuly.github.io/registration2.html"):
         browser = webdriver.Chrome()
         browser.get(url)
-        input1 = browser.find_element_by_css_selector("div.first_block > div.form-group.first_class > input")
+        input1 = browser.find_element(By.CSS_SELECTOR, ".first_block .first")
         input1.send_keys("Ivan")
-        input2 = browser.find_element_by_css_selector("div.first_block > div.form-group.second_class > input")
+        input2 = browser.find_element(By.CSS_SELECTOR, ".first_block .second")
         input2.send_keys("Petrov")
-        input3 = browser.find_element_by_css_selector("div.form-group.third_class > input")
+        input3 = browser.find_element(By.CSS_SELECTOR, ".first_block .third")
         input3.send_keys("usr@i.ua")
-        button = browser.find_element_by_css_selector("button.btn")
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
         button.click()
         time.sleep(1)
-        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
         welcome_text = welcome_text_elt.text
         self.assertEqual("Congratulations! You have successfully registered!", welcome_text,
                          msg='Registration error!!!')
 
 if __name__ == "__main__":
     unittest.main()
-
