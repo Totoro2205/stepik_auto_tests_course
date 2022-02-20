0ddb91dd42d20321253b6a45eea94dc097fdd5f3
Totoro2205 <alexander.keepsolid@gmail.com>
2022-02-20 02:55:52 +0200

    lesson 6 fixtures

---
+++ module3/test_les6_step3.py
@@ -0,0 +1,38 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import math
+
+@pytest.fixture(scope="function")
+def browser():
+    browser = webdriver.Chrome()
+    yield browser
+    browser.quit()
+
+@pytest.mark.parametrize('url', ("https://stepik.org/lesson/236895/step/1",
+                                 "https://stepik.org/lesson/236896/step/1",
+                                 "https://stepik.org/lesson/236897/step/1",
+                                 "https://stepik.org/lesson/236898/step/1",
+                                 "https://stepik.org/lesson/236899/step/1",
+                                 "https://stepik.org/lesson/236903/step/1",
+                                 "https://stepik.org/lesson/236904/step/1",
+                                 "https://stepik.org/lesson/236905/step/1"))
+def test_solver(browser, url):
+    browser.get(url)
+    input = WebDriverWait(browser, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".textarea"))
+    )
+    answer = str(math.log(int(time.time())))
+    input.send_keys(answer)
+    time.sleep(1)
+    button = WebDriverWait(browser, 3).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "button.submit-submission"))
+    )
+    button.click()
+    fbmessage = WebDriverWait(browser, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "pre.smart-hints__hint"))
+    )
+    assert fbmessage.text == "Correct!", f'Got :{fbmessage.text}'
